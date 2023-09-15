e0ab3b3cd7113261f22b16c90e0c3d8ba98e8207
Hongaproject <tkwlscjq99@gmail.com>
2023-09-15 22:01:08 +0900

    서버 파일 설명 작성

--- server/index.js
+++ server/index.js
@@ -1,23 +1,28 @@
-const express = require("express");
+// 서버와 통신이 되는지 확인 
+const express = require("express"); 
 const app = express();
-const mysql = require("mysql");
 const PORT = process.env.port || 8000;
 
-const dbConnect = mysql.createPool({ 
+// DB 연동시 위한 코드 
+const mysql = require("mysql");
+
+const dbConnect = mysql.createPool({ // DB 연동시 위한 코드 
   host: "localhost",
   user: "root",
   password: "1234",
   database: "bbs",
 });
 
-app.get("/", (req, res) => {
-  const sqlQuery = "INSERT INTO chech (test) VALUES (1)"; 
+app.get("/", (req, res) => { // 서버와 통신이 되는지 확인 
+  //console.log("chech."); // 서버와 통신이 되는지 확인 
+  
+  const sqlQuery = "INSERT INTO chech (test) VALUES (1)"; // DB 연동시 위한 코드 
   dbConnect.query(sqlQuery, (err, result) => {
     console.log(err);
     res.send("success!");
   });
 });
 
-app.listen(PORT, ()=>{
+app.listen(PORT, ()=>{ // 서버와 통신이 되는지 확인 
     console.log(`running on port ${PORT}`);
 });
