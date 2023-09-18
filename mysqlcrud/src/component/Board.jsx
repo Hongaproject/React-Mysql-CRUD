70c480bd61e2f20f1e02f92ab4b26e0c8e41fba0
Hongaproject <tkwlscjq99@gmail.com>
2023-09-18 22:45:20 +0900

    mysql테이블 작성 중 및 테이블 내용 컴포넌트 생성

---
+++ mysqlcrud/src/component/Board.jsx
@@ -0,0 +1,12 @@
+
+function Board () {
+    // table에 tbody부분에 mysql과 연동을 시켜 내용을 보여줌.
+
+    return(
+        <div>
+
+        </div>
+    );
+}
+
+export default Board;
--- mysqlcrud/src/component/TableList.jsx
+++ mysqlcrud/src/component/TableList.jsx
@@ -29,6 +29,7 @@ function TableList () {
                     </tr>
                 </thead>
                 <tbody>
+                    {/* onSubmit.map */}
                     <tr>
                         <th><input type="checkbox" /></th>
                         <td>1</td>
