3f9cd22637cbf60814bf71943914b8d4a131323c
Hongaproject <tkwlscjq99@gmail.com>
2023-09-24 22:58:24 +0900

    리뉴얼 작업 중 파일 작업중

--- mysqlcrud/src/App.jsx
+++ mysqlcrud/src/App.jsx
@@ -11,6 +11,8 @@ function App() {
       bootstrap을 사용해서 게시판 디자인을 함.
       {/* <TableList></TableList>
       <Write></Write> */}
+
+      {/* 오류 발생 및 구조 변경을 위해 리뉴얼 작업 중 */}
       <TestContents />
       <TestList />
 
