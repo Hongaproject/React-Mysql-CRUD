142eda0768d55fda4c9f5fddf23dfb4c8231321f
Hongaproject <tkwlscjq99@gmail.com>
2023-09-26 15:11:17 +0900

    리뉴얼 Write 부분 틀 작업

--- mysqlcrud/src/component/TestWrite.jsx
+++ mysqlcrud/src/component/TestWrite.jsx
@@ -1,10 +1,18 @@
 import Axios from "axios";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { Button } from "react-bootstrap";
 import { Form } from "react-bootstrap";
 
 function TestWrite({ onSubmit }) {
     // 구조를 바꿔서 작성을 해보기 위해 테스트 파일 진행 중.
+
+    const titleWrite = useRef(); // Ref를 사용하여 접근하려고 사용함.
+    const contentWrite = useRef();
+
+    const onInsert = () => {
+
+    }
+
 
     return(
         <div>
@@ -16,20 +24,22 @@ function TestWrite({ onSubmit }) {
                         name="title"
                         class="form-control" 
                         placeholder="제목을 입력해주세요." 
+                        ref={titleWrite}
                     /> 
                 </div>
                 <div class="form-group mb-3">
                     <label>내용</label>
                     <input 
-                        type="textarea" 
+                        type="text" 
                         name="content"
                         class="form-control" 
                         placeholder="내용을 입력해주세요." 
+                        ref={contentWrite}
                     />
                 </div>
             </Form>
             <div className="text-center">
-                <Button variant="primary">작성하기</Button>
+                <Button variant="primary" onClick={onInsert}>작성하기</Button>
                 <Button variant="warning">취소</Button>
             </div>
         </div>
