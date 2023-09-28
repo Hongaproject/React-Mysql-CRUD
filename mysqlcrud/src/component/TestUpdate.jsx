25c99448299e7e7e517136b9f1675767fd0f01b0
Hongaproject <tkwlscjq99@gmail.com>
2023-09-28 21:36:16 +0900

    글 수정하는 코드 작성

---
+++ mysqlcrud/src/component/TestUpdate.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+function TestUpdate ({ list, setList, onUpdate}) {
+    // 글 수정하는 컴포넌트.
+    return(
+        <div>
+
+        </div>
+    );
+}
+
+export default TestUpdate;
